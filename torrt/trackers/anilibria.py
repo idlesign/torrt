795b0eda4dba709e9d14a494962bbbafa11775da
alex <alex.a.litvinenko@gmail.com>
2019-02-17 13:33:07 +0200

    AniLibria.tv team makes typos in video quality string on regular basis: 1. fuzzy matching 2. fallback to something is better than nothing

--- torrt/trackers/anilibria.py
+++ torrt/trackers/anilibria.py
@@ -7,12 +7,17 @@ from torrt.utils import TrackerClassesRegistry
 LOGGER = logging.getLogger(__name__)
 
 REGEX_QUALITY = re.compile(r".+\[(.+)\]")
+# This regex is used to remove every non-word character or underscore from quality string.
+REGEX_NON_WORD = re.compile(r'[\W_]')
 
 
 class AnilibriaTracker(GenericPublicTracker):
     """This class implements .torrent files downloads for https://www.anilibria.tv tracker."""
 
     alias = 'anilibria.tv'
+    test_urls = [
+        'https://www.anilibria.tv/release/sword-art-online-alicization.html'
+    ]
 
     def __init__(self, quality_prefs=None):
         super(AnilibriaTracker, self).__init__()
@@ -31,7 +36,7 @@ class AnilibriaTracker(GenericPublicTracker):
             quality_str = quality_td[0].text.strip()
             match = REGEX_QUALITY.search(quality_str)
             if match:
-                quality = match.group(1)
+                quality = self.sanitize_quality(match.group(1))
                 link = self.expand_link(url, row.select('td.torrentcol4 a.torrent-download-link')[0]['href'])
                 available_qualities[quality] = link
             else:
@@ -40,9 +45,13 @@ class AnilibriaTracker(GenericPublicTracker):
         LOGGER.debug('Available in qualities: %s', ', '.join(available_qualities.keys()))
 
         if available_qualities:
-            preferred_qualities = [quality for quality in self.quality_prefs if quality in available_qualities]
+            quality_prefs = {self.sanitize_quality(pref) for pref in self.quality_prefs}
+            preferred_qualities = [quality for quality in quality_prefs if quality in available_qualities]
             if not preferred_qualities:
-                LOGGER.debug('Torrent is not available in preferred qualities: %s', ', '.join(self.quality_prefs))
+                LOGGER.info('Torrent is not available in preferred qualities: %s', ', '.join(quality_prefs))
+                quality, link = next(iter(available_qualities.items()))
+                LOGGER.info('Fallback to `%s` quality ...', quality)
+                return link
             else:
                 target_quality = preferred_qualities[0]
                 LOGGER.debug('Trying to get torrent in `%s` quality ...', target_quality)
@@ -50,6 +59,26 @@ class AnilibriaTracker(GenericPublicTracker):
                 return available_qualities[target_quality]
 
         return None
+
+    @staticmethod
+    def sanitize_quality(quality_str):
+        """
+        Turn passed quality_str into common format in order to simplify comparison.
+        Examples:
+
+        * `sanitize_quality('WEBRip 1080p')` -> 'webrip1080p'
+        * `sanitize_quality('WEBRip-1080p')` -> 'webrip1080p'
+        * `sanitize_quality('WEBRip_1080p')` -> 'webrip1080p'
+        * `sanitize_quality('')` -> ''
+        * `sanitize_quality(None)` -> ''
+
+        :type quality_str: Optional[str]
+        :param quality_str: originally extracted quality string with non-word characters
+        :return: quality string without non-word characters in lower-case
+        """
+        if quality_str:
+            return REGEX_NON_WORD.sub('', quality_str).lower()
+        return ''
 
 
 TrackerClassesRegistry.add(AnilibriaTracker)
