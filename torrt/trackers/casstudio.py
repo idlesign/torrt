bc3d760b4148d6d6772bdfb92d84f8ab68e2ecd9
Anton Bazhanov <anton.bazhanov@gmail.com>
2019-03-17 14:03:04 +0300

    casstudio login problem

--- torrt/trackers/casstudio.py
+++ torrt/trackers/casstudio.py
@@ -36,14 +36,17 @@ class CasstudioTracker(GenericPrivateTracker):
         )
 
         domain = self.extract_domain(url)
+        is_anonymous = self.find_links(url, page_soup, r'\./ucp\.php\?mode=login') is not None
 
-        if not self.logged_in:
+        if not self.logged_in or is_anonymous:
+            self.logged_in = False
             self.login(domain)
             page_soup = self.get_response(
                 url, referer=url, cookies=self.cookies, query_string=self.query_string, as_soup=True
             )
-        download_link = self.expand_link(url, page_soup.find('a', text='Скачать торрент')['href'])
-        return download_link
+        download_tag = page_soup.find('a', text='Скачать торрент')
+        if download_tag:
+            return self.expand_link(url, download_tag['href'])
 
 
 TrackerClassesRegistry.add(CasstudioTracker)
