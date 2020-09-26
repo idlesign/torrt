6f8f01a46f447a596f1e362ddcec6a7a6081f482
idle sign <idlesign@yandex.ru>
2020-09-26 08:23:20 +0700

    Remove 'six' mention.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,40 +1,17 @@
-import pytest
-
-import six
 import torrt.utils as utils
 
 
-if six.PY3:
+def test_base64encode_str():
+    string_to_encode = 'this is string'
+    encoded_bytes = utils.base64encode(string_to_encode)
 
-    def test_base64encode_str():
-        string_to_encode = 'this is string'
-        encoded_bytes = utils.base64encode(string_to_encode)
-
-        assert isinstance(encoded_bytes, bytes)
-        assert encoded_bytes == b'dGhpcyBpcyBzdHJpbmc=\n'
+    assert isinstance(encoded_bytes, bytes)
+    assert encoded_bytes == b'dGhpcyBpcyBzdHJpbmc=\n'
 
 
-    def test_base64encode_bytes():
-        bytes_to_encode = b'this is bytes to encode'
-        encoded_bytes = utils.base64encode(bytes_to_encode)
+def test_base64encode_bytes():
+    bytes_to_encode = b'this is bytes to encode'
+    encoded_bytes = utils.base64encode(bytes_to_encode)
 
-        assert isinstance(encoded_bytes, bytes)
-        assert encoded_bytes == b'dGhpcyBpcyBieXRlcyB0byBlbmNvZGU=\n'
-
-
-if six.PY2:
-
-    def test_base64encode_str():
-        string_to_encode = 'this is string'
-        encoded_string = utils.base64encode(string_to_encode)
-
-        assert isinstance(encoded_string, str)
-        assert encoded_string == "dGhpcyBpcyBzdHJpbmc=\n"
-
-
-    def test_base64encode_bytes():
-        bytes_to_encode = b'this is bytes to encode'
-        encoded_string = utils.base64encode(bytes_to_encode)
-
-        assert isinstance(encoded_string, str)
-        assert encoded_string == 'dGhpcyBpcyBieXRlcyB0byBlbmNvZGU=\n'
+    assert isinstance(encoded_bytes, bytes)
+    assert encoded_bytes == b'dGhpcyBpcyBieXRlcyB0byBlbmNvZGU=\n'
